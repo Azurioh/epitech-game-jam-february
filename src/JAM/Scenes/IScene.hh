454b1393822fcfa6884fff23293fbca4471455c4
Azurioh <alan.cunin@epitech.eu>
2025-02-07 22:02:03 +0100

    feat: Add the Scene interface

---
+++ src/JAM/Scenes/IScene.hh
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** epitech-game-jam-january
+** File description:
+** IScene
+*/
+
+#pragma once
+
+namespace Game {
+    class IScene {
+        public:
+            IScene() = default;
+            virtual ~IScene() = default;
+
+            virtual void exec(void) = 0;
+            virtual void display(void) = 0;
+            virtual void loadModel(void) = 0;
+    };
+}
---
+++ src/JAM/Scenes/SceneFactory.cpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2025
+** epitech-game-jam-january
+** File description:
+** SceneFactory
+*/
+
+#include "SceneFactory.hpp"
+
+SceneFactory::SceneFactory()
+{
+}
+
+SceneFactory::~SceneFactory()
+{
+}
+
+std::unique_ptr<void> Game::SceneFactory::testFunction()
+{
+    return;
+}
