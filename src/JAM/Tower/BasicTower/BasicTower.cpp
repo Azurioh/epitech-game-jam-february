6bb20fb89cd91076e9e89be235807de961f0f3b4
Azurioh <alan.cunin@epitech.eu>
2025-02-08 17:53:18 +0100

    feat: Add texture for BasicTower

--- src/JAM/Tower/BasicTower/BasicTower.cpp
+++ src/JAM/Tower/BasicTower/BasicTower.cpp
@@ -8,12 +8,15 @@
 #include "BasicTower.hh"
 
 Game::Tower::BasicTower::BasicTower(std::tuple<std::size_t, std::size_t> position):
-    ATower(position, 25, 10, 5, 2)
+    ATower(position, 25, 60, 5, 2)
 {
+    Image towerImage = LoadImage("asset/towers/basic.png");
     std::vector<std::tuple<unsigned int, unsigned int>> rangeSkillPricing = {{50, 5}, {100, 5}, {150, 5}};
     std::vector<std::tuple<unsigned int, unsigned int>> damageSkillPricing = {{50, 5}, {150, 10}, {300, 20}};
     std::vector<std::tuple<unsigned int, unsigned int>> attackSpeedSkillPricing = {{100, 2}, {200, 5}, {500, 10}};
 
+    _towerTexture = LoadTextureFromImage(towerImage);
+    _displayHitbox = true;
     setRangeSkillPricing(rangeSkillPricing);
     setDamageSkillPricing(damageSkillPricing);
     setAttackSpeedSkillPricing(attackSpeedSkillPricing);
@@ -21,6 +24,7 @@ Game::Tower::BasicTower::BasicTower(std::tuple<std::size_t, std::size_t> positio
 
 Game::Tower::BasicTower::~BasicTower()
 {
+    UnloadTexture(_towerTexture);
 }
 
 std::unique_ptr<Game::Projectile::IProjectile> Game::Tower::BasicTower::createProjectile(std::tuple<std::size_t, std::size_t> towerPos, std::shared_ptr<Game::Mob::IMob> target, unsigned int speed)
