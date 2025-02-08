62c863e182cdff9de4f164a0689dc6cfd919bcfe
Thomas-furstenberger <Thomas.furstenberger@epitech.eu>
2025-02-08 21:05:58 +0100

    feat: add a scene (gameScene)

---
+++ src/JAM/Scenes/GameScene/GameScene.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2025
+** epitech-game-jam-january
+** File description:
+** GameScene
+*/
+
+#ifndef GAMESCENE_HPP_
+#define GAMESCENE_HPP_
+
+    #include "JAM/JAM.hpp"
+    #include "../Buttons/Buttons.hh"
+
+namespace Game {
+    class GameScene : public Game::IScene {
+        public:
+            GameScene();
+            ~GameScene();
+            void display();
+            void exec(std::size_t &currentScene);
+
+        protected:
+        private:
+            Texture2D _frame;
+            Texture2D _heart;
+            Texture2D _coin;
+            Texture2D _board;
+    };
+}
+#endif /* !GAMESCENE_HPP_ */
