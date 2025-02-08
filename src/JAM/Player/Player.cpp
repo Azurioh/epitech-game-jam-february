07169ecaa6100527cc2f9c75a6837b76ebdf11e1
Azurioh <alan.cunin@epitech.eu>
2025-02-08 01:37:06 +0100

    feat: Add the class Player

---
+++ src/JAM/Player/Player.cpp
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2025
+** epitech-game-jam-january
+** File description:
+** Player
+*/
+
+#include "Player.hh"
+
+Game::Player::Player()
+{
+}
+
+Game::Player::~Player()
+{
+}
+
+unsigned int Game::Player::getHP() const
+{
+	return _hp;
+}
+
+unsigned int Game::Player::getGolds() const
+{
+	return _golds;
+}
+
+std::vector<std::shared_ptr<Game::Tower::ITower>> Game::Player::getTowers() const
+{
+	return _towers;
+}
+
+void Game::Player::setHP(unsigned int hp)
+{
+    _hp = hp;
+}
+
+void Game::Player::setGolds(unsigned int golds)
+{
+    _golds = golds;
+}
+
+void Game::Player::addTower(std::shared_ptr<Game::Tower::ITower> &tower)
+{
+    _towers.push_back(tower);
+}
+
+void Game::Player::sellTower(std::shared_ptr<Game::Tower::ITower> &tower)
+{
+    _towers.erase(std::remove(_towers.begin(), _towers.end(), tower), _towers.end());
+}
