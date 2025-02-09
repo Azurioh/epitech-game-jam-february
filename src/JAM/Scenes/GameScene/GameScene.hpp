7674b8bab9327754dfefab25d5accdb236c00bec
Thomas-furstenberger <Thomas.furstenberger@epitech.eu>
2025-02-09 02:08:09 +0100

    feat: add button for buy tower (shop)

--- src/JAM/Scenes/GameScene/GameScene.hpp
+++ src/JAM/Scenes/GameScene/GameScene.hpp
@@ -24,11 +24,20 @@ namespace Game {
         private:
             int _hp;
             int _gold;
+            const float REF_WIDTH;
+            const float REF_HEIGHT;
             std::unique_ptr<Player> _player;
             Texture2D _frame;
+            Buttons _T1;
+            Buttons _T2;
+            Buttons _T3;
+            Buttons _T4;
+            Buttons _T5;
             Texture2D _heart;
             Texture2D _coin;
             Texture2D _board;
+            float _T1PercentX;
+            float _T1PercentY;
     };
 }
 #endif /* !GAMESCENE_HPP_ */
